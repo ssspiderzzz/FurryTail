69629483cfa5de495b2ba65dc4a5fbb3bb1a379b
Haibin Zhu <ssspiderzzz@gmail.com>
2021-12-10 22:02:29 -0800

    add short and long buttons, and scale var

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 
 export default function App() {
   const [clipVar, setClipVar] = useState(80);
-  const [marginVar, setMarginVar] = useState(40);
+  const [marginVar, setMarginVar] = useState(55);
+  const [scaleVar, setScaleVar] = useState(1.5);
 
   const clipPathValue = `polygon(${clipVar}% 0, 100% 0, 100% 100%, ${clipVar}% 100%)`;
   const marginValue = `${marginVar}%`;
+  const scaleValue = `scale(${scaleVar})`;
 
   const animate = () => {
     setClipVar(0);
     setMarginVar(0);
+    setScaleVar(1);
+  };
+
+  const animateToShort = () => {
+    setClipVar(80);
+    setMarginVar(55);
+    setScaleVar(1.5);
   };
 
   return (
     <div className="App">
-      <button onClick={() => animate()}>FurryTail</button>
+      <button onClick={() => animate()}>LongFurryTail</button>
+      <button onClick={() => animateToShort()}>ShortFurryTail</button>
       <br />
       <img
         src="/assets/furrytail.png"
         alt="FurryTail"
-        style={{ position: "absolute", width: "100%", right: marginValue, clipPath: clipPathValue, transition: "all 2s ease" }}
+        style={{ position: "absolute", width: "100%", right: marginValue, clipPath: clipPathValue, transition: "all 2s ease", transform: scaleValue }}
       />
     </div>
   );
